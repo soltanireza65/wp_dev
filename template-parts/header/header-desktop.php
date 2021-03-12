3d63d248f0f72094c839eadbb7ec66bcbd360d42
Pooya-sz <pooyasz71@gmail.com>
2021-03-12 20:01:54 +0330

    change the logo

--- template-parts/header/header-desktop.php
+++ template-parts/header/header-desktop.php
@@ -1,62 +1,74 @@
 <header id="" class="site-header">
-    <div class="app_top_header d-flex justify-content-around justify-content-lg-between align-items-center container">
-        <div class="site-branding">
-            <?php the_custom_logo(); ?>
-        </div>
-        <div class="header_search_form_wraper w-50">
-            <?php _themename_woo_product_search(); ?>
-        </div>
+    <div class="app_top_header  container">
+        <div class="row">
+            <div class="col-md-3 text-right">
+                <div class="site-branding">
+                    <?php the_custom_logo(); ?>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="header_search_form_wraper w-50">
+                    <?php _themename_woo_product_search(); ?>
+                </div>
+            </div>
+            <div class="col-md-3 mr-auto">
+                <div class="d-flex position-relative">
+                    <div class="d-flex align-items-center p-0 px-1 text-secondary mr-auto">
+                        <div class="user_links_dropdown mr-3 ">
+                            <a href="#" class="dropbtn app_woo_user_icon"><i class="fal fa-user  fontsize_17 circle_padding_white"></i></a>
 
-        <div class="d-flex position-relative">
-            <div class="d-flex align-items-center p-0 px-1 text-secondary">
-                <div class="user_links_dropdown mr-3 ">
-                    <a href="#" class="dropbtn app_woo_user_icon"><i class="fal fa-user  fontsize_17 circle_padding_white"></i></a>
+                            <div class=" text-center overflow-hidden user_links_dropdown-content text-right border-0 radius app_auth_urls" aria-labelledby="dropdownMenuButton">
 
-                    <div class=" text-center overflow-hidden user_links_dropdown-content text-right border-0 radius app_auth_urls" aria-labelledby="dropdownMenuButton">
+                                <?php if (is_user_logged_in()) : ?>
+                                    <?php
+                                    $myaccount_page_id = get_option('woocommerce_myaccount_page_id');
+                                    if ($myaccount_page_id) {
+                                        $myaccount_page_url = get_permalink($myaccount_page_id);
+                                    }
+                                    ?>
+                                    <a class="dropdown-item" href="<?php echo $myaccount_page_url ?>">حساب کاربری</a>
+                                    <?php
+                                    $store_user = dokan()->vendor->get(get_query_var('author'));
+                                    // var_dump($store_user);
+                                    $user = wp_get_current_user();
+                                    if (in_array('seller', (array) $user->roles)) {
+                                    ?>
+                                        <a class="dropdown-item seller_dash" href="<?php echo get_site_url() ?>/shop_proj/dashboard">پنل فروشندگان</a>
+                                    <?php
+                                    }
+                                    ?>
+                                    <?php wp_loginout(); ?>
+                                <?php else : ?>
+                                    <a class="text-center dropdown-item" href="<?php echo get_site_url() ?>/shop_proj/my-account/">ورود/ ثبت نام</a>
 
-                        <?php if (is_user_logged_in()) : ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+
+
+                        <a class="ml-3 d-none d-md-block app_woo_wishlist_icon" href="<?php echo get_site_url() . '/wishlist'; ?>">
+                            <i class="fal fa-heart fontsize_17 icon-size-md circle_padding_white"></i>
+                        </a>
+
+                        <div class="app_woo_mini_cart">
                             <?php
-                            $myaccount_page_id = get_option('woocommerce_myaccount_page_id');
-                            if ($myaccount_page_id) {
-                                $myaccount_page_url = get_permalink($myaccount_page_id);
+                            if (function_exists('_themename_woocommerce_header_cart')) {
+                                _themename_woocommerce_header_cart();
                             }
                             ?>
-                            <a class="dropdown-item" href="<?php echo $myaccount_page_url ?>">حساب کاربری</a>
-                            <?php
-                            $store_user = dokan()->vendor->get(get_query_var('author'));
-                            // var_dump($store_user);
-                            $user = wp_get_current_user();
-                            if (in_array('seller', (array) $user->roles)) {
-                            ?>
-                                <a class="dropdown-item seller_dash" href="<?php echo get_site_url() ?>/shop_proj/dashboard">پنل فروشندگان</a>
-                            <?php
-                            }
-                            ?>
-                            <?php wp_loginout(); ?>
-                        <?php else : ?>
-                            <a class="text-center dropdown-item" href="<?php echo get_site_url() ?>/shop_proj/my-account/">ورود/ ثبت نام</a>
+                        </div>
 
-                        <?php endif; ?>
+
                     </div>
                 </div>
-
-
-
-                <a class="ml-3 d-none d-md-block app_woo_wishlist_icon" href="<?php echo get_site_url() . '/wishlist'; ?>">
-                    <i class="fal fa-heart fontsize_17 icon-size-md circle_padding_white"></i>
-                </a>
-
-                <div class="app_woo_mini_cart">
-                    <?php
-                    if (function_exists('_themename_woocommerce_header_cart')) {
-                        _themename_woocommerce_header_cart();
-                    }
-                    ?>
-                </div>
-
-
             </div>
         </div>
+
+
+
+
+
     </div>
     <nav id="site-navigation" class="site-navigation container" role="navigation">
         <?php
