b9ee3e2cbb402f976c6d27d92821b3b5d61a7232
Reza <soltanireza65@gmail.com>
2021-04-11 17:11:05 +0430

    search-form template

--- template-parts/header/header-desktop.php
+++ template-parts/header/header-desktop.php
@@ -8,40 +8,10 @@
             </div>
             <div class="col-md-6 header_search_form_wraper position-relative" style="z-index: 33;">
                 <!-- ============================================================= -->
-                <form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url(home_url('/')); ?>">
-                    <input class="product_search_term" type="text" id="keyword" name="s"></input>
-                    <?php
+                <?php
+                get_template_part('template-parts/header/search-form');
 
-                    $product_cats = getProductCategories();
-                    if (!empty($product_cats) && !is_wp_error($product_cats)) :
-                        $selected_product_cat = get_query_var('product_cat');
-                    ?>
-                        <select name="product_cat" id="product_cat" class="product_cat">
-                            <option value="" disabled selected>دسته بندی ها</option>
-                            <?php
-                            foreach ($product_cats as $product_cat) {
-                            ?>
-                                <option value="<?php echo esc_attr($product_cat->slug) ?>" <?php selected($product_cat->slug, $selected_product_cat) ?>><?php echo esc_html($product_cat->name); ?></option>
-                            <?php
-                            }
-                            ?>
-                        </select>
-                    <?php endif; ?>
-                    <button class="product_search_btn" id="product_search_btn" type="submit" value="جستجو">
-                        <i class="fad fa-search"></i>
-                    </button>
-
-                    <input type="hidden" id="" name="post_type" value="product" />
-                </form>
-                <div class="result" id="result">
-                    <ul class="products">
-                        <li class="d-flex justify-content-between">
-                            <img src="" alt="">
-                            <h5>titleeeeeeeeeeee</h5>
-                            <p>4444444</p>
-                        </li>
-                    </ul>
-                </div>
+                ?>
                 <!-- ============================================================= -->
             </div>
             <div class="col-md-3 d-flex justify-content-end align-items-center position-relative">
